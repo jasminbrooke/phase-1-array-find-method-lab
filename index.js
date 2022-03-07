d7dacb8ad381214c17979c16195a9e3b853c30b6
+ <+>
2022-03-07 12:31:05 -0500

    commit tree 41a120d6320db377ed5553fd504236fbe735ef9d

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+// function superbowlWin(record){
+//     let result = record.find(function(element){
+//         return (element.result === "W")
+//     })
+//     if (result === "N/A" ||  result === "L")
+//         return result
+//     else
+//         return result.year
+// }
+
+function superbowlWin(record){
+    let result = record.find(function(element){
+        return (element.result === "W")
+    })
+    if (result)
+        return result.year
+    else
+        return result
+}
